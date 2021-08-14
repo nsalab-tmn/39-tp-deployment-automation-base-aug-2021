242e59f6fe08f894e0af22307685b14f1b427ce2
menus12 <menus12@mail.ru>
2021-08-14 19:54:17 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 ansible-inventory --graph --yaml --vars
 ansible -m ping all 
 ansible-playbook deploy.yaml
-mkdir output
 ansible-playbook audit.yaml
 python3 combine_output.py --input-dir ./output --output-file ./audit_results.yaml
